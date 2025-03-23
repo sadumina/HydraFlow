5d8560f84528577a1c1669ea2120e0b239660f59
Sadumina Rathnayaka <IT23394124@my.sliit.lk>
2025-03-23 12:29:22 +0530

    complete login page ui

---
+++ app/src/main/java/com/example/hydraflow/login page.kt
@@ -0,0 +1,13 @@
+package com.example.hydraflow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class `login page` : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        }
+    }
