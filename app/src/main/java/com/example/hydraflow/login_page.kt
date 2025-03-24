5d3c90a8e412c24c8383a993ff392814bc3472b2
Sadumina Rathnayaka <IT23394124@my.sliit.lk>
2025-03-24 16:26:19 +0530

    testing

--- app/src/main/java/com/example/hydraflow/login_page.kt
+++ app/src/main/java/com/example/hydraflow/login_page.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class `login page` : AppCompatActivity() {
+class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
