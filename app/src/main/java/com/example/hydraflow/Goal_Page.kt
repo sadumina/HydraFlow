a6ef46e277b6666a0b9ce47b639f493488636992
Sadumina Rathnayaka <IT23394124@my.sliit.lk>
2025-03-24 12:05:08 +0530

    begin to implement the Goal_Page

---
+++ app/src/main/java/com/example/hydraflow/Goal_Page.kt
@@ -0,0 +1,20 @@
+package com.example.hydraflow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Goal_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_goal_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
